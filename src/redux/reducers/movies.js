a1ee016f6518487bc3bf3c24099589896200bec2
Hoorzy <nazari.hoorie@gmail.com>
2020-01-23 15:35:58 +0100

    add

--- src/redux/reducers/movies.js
+++ src/redux/reducers/movies.js
@@ -2,8 +2,33 @@ import { RATE } from "../types";
 
 const initialState = [
   {
+    image: "TheIrishman.jpg",
+    name: "The Irishman",
+    rate: 0
+  },
+  {
+    image: "marriagestory.jpg",
+    name: "Marriage Story",
+    rate: 0
+  },
+  {
+    image: "1917.jpg",
+    name: "1917",
+    rate: 0
+  },
+  {
+    image: "parasite.jpg",
+    name: "Parasite",
+    rate: 0
+  },
+  {
     image: "antman.jpg",
     name: "Ant Man",
+    rate: 0
+  },
+  {
+    image: "littlewomen.jpg",
+    name: "littlewomen",
     rate: 0
   },
   {
@@ -12,18 +37,13 @@ const initialState = [
     rate: 0
   },
   {
-    image: "captainamerica.jpg",
-    name: "Captain America",
+    image: "starwars.jpg",
+    name: "Star Wars",
     rate: 0
   },
   {
-    image: "deadpool.jpg",
-    name: "Dead Pool",
-    rate: 0
-  },
-  {
-    image: "it.jpg",
-    name: "It Capter 2",
+    image: "onceuponatimeinhollywood.jpg",
+    name: "Once Upon a Time in Hollywood",
     rate: 0
   },
   {
@@ -36,6 +56,8 @@ const initialState = [
     name: "Terminator",
     rate: 0
   }
+ 
+  
 ];
 
 export default (state = initialState, action) => {
